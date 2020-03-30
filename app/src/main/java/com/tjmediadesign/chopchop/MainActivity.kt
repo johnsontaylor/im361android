a2f53be4caecc5422f8a950c176d3a89e67812b0
johnsontaylor <60371093+johnsontaylor@users.noreply.github.com>
2020-03-30 18:24:15 -0500

    Initial Commit

---
+++ app/src/main/java/com/tjmediadesign/chopchop/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tjmediadesign.chopchop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
