7b38b54dd343461809c42268d50b0e4fc1214fb4
johnsontaylor <60371093+johnsontaylor@users.noreply.github.com>
2020-04-06 18:20:30 -0500

    Fizz Buzz Test

--- app/src/main/java/com/tjmediadesign/chopchop/MainActivity.kt
+++ app/src/main/java/com/tjmediadesign/chopchop/MainActivity.kt
@@ -2,11 +2,26 @@ package com.tjmediadesign.chopchop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.text_view)
+        textView.text = "Fizz Fizz Buzz Buzz ${fizzBuzz()}"
     }
+
+    private fun fizzBuzz() {
+        for (i in 1..100) {
+            if (i % 3 == 0) print("SheLookedSurprised ")
+            if (i % 5 == 0) print("IToldMyWifeSheWasDrawing ")
+            if (i % 3 != 0 && i % 5 !=0 ) print("HerEyebrowsTooHigh")
+        }
+        }
+
+
+
 }
