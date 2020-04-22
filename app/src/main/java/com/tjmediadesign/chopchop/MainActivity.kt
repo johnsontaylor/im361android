7ab7ccd077052e4eee93569e872ad669cdc1d413
johnsontaylor <60371093+johnsontaylor@users.noreply.github.com>
2020-04-22 18:35:18 -0500

    Tab Icon Functions

--- app/src/main/java/com/tjmediadesign/chopchop/MainActivity.kt
+++ app/src/main/java/com/tjmediadesign/chopchop/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.tjmediadesign.chopchop
 
+import android.accounts.Account
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : FragmentActivity() {
     lateinit var viewPager: ViewPager2
@@ -18,13 +22,50 @@ class MainActivity : FragmentActivity() {
         val adapter = MainActivityPagerAdapter(this)
         viewPager.adapter = adapter
 
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+        val whiteAccent = ContextCompat.getColor(this, R.color.whiteAccent)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            if (position == 0) {
+                tab.text = "Restaurants"
+
+                tab.setIcon(R.drawable.resticon)
+            }
+            if (position == 1) {
+                tab.text = "My Plate"
+                tab.setIcon(R.drawable.plateicon)
+            }
+            if (position == 2) {
+                tab.text = "Account"
+                tab.setIcon(R.drawable.accicon)
+            }
+            tab.icon?.setColorFilter(whiteAccent, PorterDuff.Mode.SRC_ATOP)
+        }.attach()
     }
+
+    override fun onBackPressed() {
+        if (viewPager.currentItem > 0) {
+            viewPager.currentItem -= 1
+            return
+        }
+
+        super.onBackPressed()
+    }
+
     private inner class MainActivityPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
         override fun getItemCount(): Int {
             return 3
         }
         override fun createFragment(position: Int): Fragment {
-            return RestListFragment()
+            if (position == 0) {
+                return RestListFragment()
+            }
+            if (position == 1) {
+                return PlateListFragment()
+            }
+
+            return AccountListFragment()
+
         }
 
     }
