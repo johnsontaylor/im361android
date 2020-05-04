dd89fb4a6fc1dc98162cc3c8002978b9e0864b12
johnsontaylor <60371093+johnsontaylor@users.noreply.github.com>
2020-05-04 18:39:47 -0500

    Update Meal.kt

--- app/src/main/java/com/tjmediadesign/chopchop/Meal.kt
+++ app/src/main/java/com/tjmediadesign/chopchop/Meal.kt
@@ -1,3 +1,8 @@
 package com.tjmediadesign.chopchop
 
-data class Meal(val name: String)
+data class Meal(
+    val food: String,
+    val price: String,
+    val store: String,
+    val bgImage: Int
+)
